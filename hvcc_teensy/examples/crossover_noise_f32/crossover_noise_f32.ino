c11243ead5bede792a46b9ab2c87607337e35faa
Thomas Grill <gr@grrrr.org>
2025-04-25 18:20:04 +0200

    Update example patch

--- hvcc_teensy/examples/crossover_noise_f32/crossover_noise_f32.ino
+++ hvcc_teensy/examples/crossover_noise_f32/crossover_noise_f32.ino
@@ -1,17 +1,62 @@
-#include <OpenAudio_ArduinoLibrary.h>
-
 #define OPENAUDIO 1
 #include <crossover_noise_f32.h>
 
+class _crossover_noise:
+  public crossover_noise_f32
+{
+protected:
+  virtual void receive(double timestampMs, const char *receiverName, const HvMessage *m) 
+  {
+    return;
+    if(!strcmp(receiverName, "bang")) {
+        printf("BANG @ %g\n", timestampMs);
+    }
+    else if(!strcmp(receiverName, "frq") && msg_isFloat(m, 0)) {
+      float f = msg_getFloat(m, 0);
+      printf("RECV @ %g - %s %f\n", timestampMs, receiverName, f);
+    }
+  }
+};
+
+
+#if OPENAUDIO
+#include <OpenAudio_ArduinoLibrary.h>
+
 AudioSynthNoiseWhite_F32 noise;
-crossover_noise_f32 xover;
+_crossover_noise xover;
+AudioConnection_F32 patch_in(noise, 0, xover, 0);
+
+AudioInputAnalog inp_frq(A0, false);
+AudioConvert_I16toF32  cnv_frq;
+AudioConnection conn_frq(inp_frq, 0, cnv_frq, 0);
+AudioConnection_F32 patch_frq(cnv_frq, 0, xover, 1);
+
 AudioOutputI2S_F32 i2sout;
-AudioConnection_F32 patchCordIn(noise, 0, xover, 0);
-AudioConnection_F32 patchCordLP(xover, 0, i2sout, 0), patchCordHP(xover, 1, i2sout, 1);
+AudioConnection_F32 patch_lp(xover, 0, i2sout, 0), patch_hp(xover, 1, i2sout, 1);
+
+#else
+
+#include <Audio.h>
+
+AudioSynthNoiseWhite noise;
+_crossover_noise xover;
+
+AudioConnection patch_in(noise, 0, xover, 0);
+
+AudioInputAnalog inp_frq(A0, false);
+AudioConnection conn_frq(inp_frq, 0, xover, 1);
+
+AudioOutputI2S i2sout;
+AudioConnection patch_lp(xover, 0, i2sout, 0), patch_hp(xover, 1, i2sout, 1);
+
+#endif
 
 void setup()
 {
+  AudioMemory(8);
+#if OPENAUDIO
   AudioMemory_F32(8);
+#endif
 
   noise.amplitude(0.1);
   xover.sendfloat("frq", 2000);
@@ -23,6 +68,11 @@ void loop()
 {
   printf("Sample rate: %.0f / Block size: %.0f\n", float(AUDIO_SAMPLE_RATE_EXACT), float(AUDIO_BLOCK_SAMPLES));
   printf("DSP usage: %.2f%% (%.2f%% max)\n", float(AudioProcessorUsage()), float(AudioProcessorUsageMax()));
-  printf("Mem usage: %d blocks (%d blocks max)\n", int(AudioMemoryUsage_F32()), int(AudioMemoryUsageMax_F32()));
+  printf("Mem usage: %d blocks", int(AudioMemoryUsageMax()));
+#if OPENAUDIO
+  printf(" / Mem_F32 usage: %d blocks\n", int(AudioMemoryUsageMax_F32()));
+#else
+  printf("\n");
+#endif
   delay(1000);
 }
