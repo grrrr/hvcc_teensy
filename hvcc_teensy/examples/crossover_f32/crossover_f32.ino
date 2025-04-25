23212384e3a48a4949cd90f599e6e6b98ae8a9e4
Thomas Grill <gr@grrrr.org>
2025-04-25 18:33:30 +0200

    Update example patch

--- hvcc_teensy/examples/crossover_f32/crossover_f32.ino
+++ hvcc_teensy/examples/crossover_f32/crossover_f32.ino
@@ -1,17 +1,32 @@
-#include <OpenAudio_ArduinoLibrary.h>
-
 #define OPENAUDIO 1
 #include <crossover_f32.h>
+
+#if OPENAUDIO
+#include <OpenAudio_ArduinoLibrary.h>
 
 AudioInputI2S_F32 i2sin;
 crossover_f32 xover;
 AudioOutputI2S_F32 i2sout;
-AudioConnection_F32 patchCordIn(i2sin, 0, xover, 0);
-AudioConnection_F32 patchCordLP(xover, 0, i2sout, 0), patchCordHP(xover, 1, i2sout, 1);
+AudioConnection_F32 patch_in(i2sin, 0, xover, 0);
+AudioConnection_F32 patch_lp(xover, 0, i2sout, 0), patch_hp(xover, 1, i2sout, 1);
+
+#else
+#include <Audio.h>
+
+AudioInputI2S i2sin;
+crossover_f32 xover;
+AudioOutputI2S i2sout;
+AudioConnection patch_in(i2sin, 0, xover, 0);
+AudioConnection patch_lp(xover, 0, i2sout, 0), patch_hp(xover, 1, i2sout, 1);
+
+#endif
 
 void setup()
 {
+  AudioMemory(8);
+#if OPENAUDIO
   AudioMemory_F32(8);
+#endif
 
   xover.sendfloat("frq", 2000);
   xover.sendfloat("hf_att", -10);
@@ -22,6 +37,11 @@ void loop()
 {
   printf("Sample rate: %.0f / Block size: %.0f\n", float(AUDIO_SAMPLE_RATE_EXACT), float(AUDIO_BLOCK_SAMPLES));
   printf("DSP usage: %.2f%% (%.2f%% max)\n", float(AudioProcessorUsage()), float(AudioProcessorUsageMax()));
-  printf("Mem usage: %d blocks (%d blocks max)\n", int(AudioMemoryUsage_F32()), int(AudioMemoryUsageMax_F32()));
+  printf("Mem usage: %d blocks", int(AudioMemoryUsageMax()));
+#if OPENAUDIO
+  printf(" / Mem_F32 usage: %d blocks\n", int(AudioMemoryUsageMax_F32()));
+#else
+  printf("\n");
+#endif
   delay(1000);
 }
